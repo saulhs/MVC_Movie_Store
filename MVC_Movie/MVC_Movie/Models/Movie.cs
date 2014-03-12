047f8d32947710ea5336e004b4f93e78a36890e0
saulhs <saulhs89@gmail.com>
2014-03-12 00:49:59 -0400

    Actualización Proyecto 12-03-14 --- Revisar Rating

--- MVC_Movie/MVC_Movie/Models/Movie.cs
+++ MVC_Movie/MVC_Movie/Models/Movie.cs
@@ -13,6 +13,7 @@ namespace MVC_Movie.Models
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 
     public class MovieDBContext : DbContext
